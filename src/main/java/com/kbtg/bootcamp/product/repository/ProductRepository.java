f58238a2dd49328f1ed2b7dda791c9c78a99eb60
Wit <t80031@email.scb.co.th>
2022-02-23 15:53:09 +0700

    [modified] feature/create_product add new method findByCategoryId at ProductRepository.

--- src/main/java/com/kbtg/bootcamp/product/repository/ProductRepository.java
+++ src/main/java/com/kbtg/bootcamp/product/repository/ProductRepository.java
@@ -2,6 +2,10 @@ package com.kbtg.bootcamp.product.repository;
 
 import com.kbtg.bootcamp.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByCategoryId(@RequestParam("id") Long id);
 }
