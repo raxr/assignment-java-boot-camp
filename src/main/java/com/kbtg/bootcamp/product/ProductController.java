f9ce3a1d2a484fda305d7b9a6988e2f8bea31cf9
Wit <t80031@email.scb.co.th>
2022-02-23 15:53:37 +0700

    [modified] feature/create_product add new method getCategory at ProductController class.

--- src/main/java/com/kbtg/bootcamp/product/ProductController.java
+++ src/main/java/com/kbtg/bootcamp/product/ProductController.java
@@ -1,4 +1,24 @@
 package com.kbtg.bootcamp.product;
 
+import com.kbtg.bootcamp.product.response.ProductResponse;
+import com.kbtg.bootcamp.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/api")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getCategory(@PathVariable Long id) {
+        ProductResponse category = productService.getCategory(id);
+        return ResponseEntity.ok(category);
+    }
 }
